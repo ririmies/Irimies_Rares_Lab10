00e1eb8fa6baae95faea9af1fa38106c2298c3e8
ririmies <rares_irimies@yahoo.com>
2021-12-11 09:40:51 +0200

    Add project files.

---
+++ Irimies_Rares_Lab10/Models/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Irimies_Rares_Lab10.Models
+{
+   public class ShopList
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
